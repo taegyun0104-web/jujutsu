d24c0dac4e573de5bfb4606a66fc7986bad2ab71
taegyun0104-web <taegyun0104@gmail.com>
2026-05-06 09:41:02 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2768,3 +2768,6 @@ client.on("interactionCreate", async (interaction) => {
 client.login(TOKEN);
 client.login(process.env.TOKEN);
 console.log("TOKEN:", process.env.TOKEN);
+client.on("messageCreate", msg => {
+  console.log("내용:", msg.content);
+});
